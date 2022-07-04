1ebed58aa595bdb591455f23c5043b8d4ffcb3a1
Douglas Obara <douglasobara97@gmail.com>
2022-07-04 23:22:04 +0300

    add navbar and hero section

---
+++ MicrosoftCloneHome/js/script.js
@@ -0,0 +1,15 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+const dropdownFun = () => {
+  document.getElementById("dropdownMenu").classList.toggle("show");
+};
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+  if (!e.target.matches(".dropbtn")) {
+    var dropdownMenu = document.getElementById("dropdownMenu");
+    if (dropdownMenu.classList.contains("show")) {
+      dropdownMenu.classList.remove("show");
+    }
+  }
+};
